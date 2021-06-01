651f35c9f674e24291e7e3baf611a1f26abffc3f
kolibre <2734079634@qq.com>
2021-06-02 01:07:05 +0800

    finish easy ast unit test, not completely

--- test/ast-unit-test/ast-unit-test.sh
+++ test/ast-unit-test/ast-unit-test.sh
@@ -4,7 +4,7 @@ var=$(pwd)
 
 flex -o ../../src/driver/scanner.cpp ../../src/driver/scanner.l
 bison -o ../../src/driver/parser.cpp -d ../../src/driver/parser.y -v
-g++ -o ./ast-unit-test.out ../../src/driver/scanner.cpp ../../src/driver/parser.cpp ../../src/ast/ast.cpp ../../src/ast/ast-unit-test.cpp -lfl -lm -I ../../src/ -DDEBUG -DAST_DEBUG
-rm ../../src/driver/scanner.cpp ../../src/driver/parser.cpp ../../src/driver/parser.hpp ../../src/driver/parser.output
+g++ -o ./ast-unit-test.out ../../src/driver/scanner.cpp ../../src/driver/parser.cpp ../../src/ast/ast.cpp ../../src/ast/ast-unit-test.cpp -lfl -lm -I ../../src/ -g -DDEBUG -DAST_DEBUG
+#rm ../../src/driver/scanner.cpp ../../src/driver/parser.cpp ../../src/driver/parser.hpp ../../src/driver/parser.output
 ./ast-unit-test.out input.txt output.txt
-rm ./ast-unit-test.out
+#rm ./ast-unit-test.out
